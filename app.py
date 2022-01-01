73cd0f05a31efecab158ec0243f5a797ae138e42
Aleksandr <yulgu987@gmail.com>
2022-01-01 23:07:59 +0300

    add login and password

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
-import sqlite3
-from werkzeug.exceptions import abort
+from api.models.post import get_post, get_db_connection
 
-def get_db_connection():
-   conn = sqlite3.connect('database.db')
-   conn.row_factory = sqlite3.Row
-   return conn
-
-
-def get_post(post_id):
-   conn = get_db_connection()
-   post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                       (post_id,)).fetchone()
-   conn.close()
-   if post is None:
-      abort(404)
-   return post
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
 @app.route("/")
 def index():
@@ -81,6 +67,9 @@ def delete(id):
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+
+
+
 
 app.config['JSON_AS_ASCII'] = False
 
